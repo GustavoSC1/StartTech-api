06e2941671ae4b41b35529db23834b368237f16b
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-03-31 21:50:47 -0300

    Project created

---
+++ src/test/java/com/gustavo/starttech/StartTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.starttech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
