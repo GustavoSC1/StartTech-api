c12c49fa0b72c5654a93cf2fef59249de73975e6
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-04-02 00:41:40 -0300

    Creating EmpresaRepository and saveEmpresaTest

---
+++ src/test/java/com/gustavo/starttech/repositories/EmpresaRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.gustavo.starttech.repositories;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.gustavo.starttech.entities.Empresa;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest
+public class EmpresaRepositoryTest {
+	
+	@Autowired
+	EmpresaRepository empresaRepository;
+	
+	@Test
+	@DisplayName("Must save a company ")
+	public void saveEmpresaTest() {		
+		Empresa newEmpresa = new Empresa(null, "Google LLC", "google@gmail.com", "1629129421", "16988218142", "51799337000141", "Google");
+				
+		Empresa savedEmpresa = empresaRepository.save(newEmpresa);
+		
+		Assertions.assertThat(savedEmpresa.getId()).isNotNull();		
+	}
+
+}
