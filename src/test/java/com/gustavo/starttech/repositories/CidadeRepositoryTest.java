c61770f4dff675d8b2826f8c12af23df6536f722
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-04-28 20:36:02 -0300

    Creating the test for the CidadeRepository findCidades

--- src/test/java/com/gustavo/starttech/repositories/CidadeRepositoryTest.java
+++ src/test/java/com/gustavo/starttech/repositories/CidadeRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.gustavo.starttech.repositories;
 
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.gustavo.starttech.entities.Cidade;
+import com.gustavo.starttech.entities.Estado;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -34,6 +37,27 @@ public class CidadeRepositoryTest {
 		
 		// Verification
 		Assertions.assertThat(savedCidade.getId()).isNotNull();
+	}
+	
+	@Test
+	@DisplayName("Must get all cities in a state sorted by name")
+	public void findCidadesTest() {
+		// Scenario
+		Estado estado = new Estado(null, "Bahia");
+		Cidade cidade = new Cidade(null, "Paramirim", estado);
+		Cidade cidade1 = new Cidade(null, "Vitória da Conquista", estado);
+				
+		entityManager.persist(estado);
+		entityManager.persist(cidade);
+		entityManager.persist(cidade1);
+		
+		// Execution
+		List<Cidade> cidades = cidadeRepository.findCidades(estado.getId());
+		
+		// Verification
+		Assertions.assertThat(cidades.size()).isEqualTo(2);
+		Assertions.assertThat(cidades.get(0).getNome()).isEqualTo(cidade.getNome());
+		Assertions.assertThat(cidades.get(1).getNome()).isEqualTo(cidade1.getNome());
 	}
 
 }
